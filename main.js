987613469e43789f12da68bf14effd5f91beded8
jamesnitz <jamesanitz2@gmail.com>
2020-09-10 08:55:58 -0500

    got heaps of conditionals and lists now

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-const app = Vue.createApp({
+const appVue = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
+            image: './assets/images/socks_blue.jpg',  
             inventory: 100,
             onSale: true,
-            cheatUrl: 'https://www.vuemastery.com/pdf/Vue-Essentials-Cheat-Sheet.pdf'
+            cheatUrl: 'https://www.vuemastery.com/pdf/Vue-Essentials-Cheat-Sheet.pdf',
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+              { id: 2234, color: 'green'},
+              { id: 2235, color: 'blue'}
+            ],
+            sizes: ['Small', 'Shmedium', 'Sizeable']
         }
     }
 })
