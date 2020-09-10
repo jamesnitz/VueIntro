3871d4dbce631d70c56a20d5061e479df0bfdfaa
jamesnitz <jamesanitz2@gmail.com>
2020-09-10 09:13:42 -0500

    lesson 6 handful o events

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const appVue = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',  
             inventory: 100,
@@ -8,10 +9,23 @@ const appVue = Vue.createApp({
             cheatUrl: 'https://www.vuemastery.com/pdf/Vue-Essentials-Cheat-Sheet.pdf',
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green'},
-              { id: 2235, color: 'blue'}
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ],
             sizes: ['Small', 'Shmedium', 'Sizeable']
         }
+    },
+    methods: {
+      addToCart() {
+        this.cart++
+        this.inventory--
+      },
+      clearCart(cartCount) {
+        this.cart -= cartCount
+        this.inventory += cartCount
+      },
+      updateImage(variantImage) {
+        this.image = variantImage
+      }
     }
 })
