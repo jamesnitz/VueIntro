d61492e385e81c2338dbd2de09061074cd6ef87c
jamesnitz <jamesanitz2@gmail.com>
2020-09-10 11:24:22 -0500

    clearing cart returns the correct quanity to the variants regardless of selectedVariant. WOO

--- main.js
+++ main.js
@@ -1,31 +1,60 @@
 const appVue = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg',  
-            inventory: 10,
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
             onSale: true,
             cheatUrl: 'https://www.vuemastery.com/pdf/Vue-Essentials-Cheat-Sheet.pdf',
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 4 },
             ],
             sizes: ['Small', 'Shmedium', 'Sizeable']
         }
     },
     methods: {
-      addToCart() {
-        this.cart++
-        this.inventory--
+      addToCart(addedItemColor) {
+        const foundProduct = this.cart.find(item => item.color === addedItemColor)
+        if (!foundProduct) {
+          this.cart.push({
+            color: addedItemColor,
+            quantity: 1
+          })
+        } else {
+          foundProduct.quantity++
+        }
+        this.variants[this.selectedVariant].quantity--
       },
-      clearCart(cartCount) {
-        this.cart= 0
-        this.inventory += cartCount
+      clearCart() {
+        for (let item of this.cart) {
+          const foundVarient = this.variants.find(varient => varient.color === item.color)
+          foundVarient.quantity += item.quantity
+        }
+        this.cart = []
       },
-      updateImage(variantImage) {
-        this.image = variantImage
+      updateVariant(index) {
+        this.selectedVariant = index
+      }
+    },
+    computed: {
+      title() {
+        return this.brand + ' ' + this.product
+      },
+      image() {
+        return this.variants[this.selectedVariant].image
+      },
+      inStock() {
+        return this.variants[this.selectedVariant].quantity
+      },
+      cartTotal() {
+        let totalQuantity = 0
+        for (let item of this.cart) {
+          totalQuantity += item.quantity
+        }
+        return totalQuantity
       }
     }
 })
